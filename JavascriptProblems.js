319f6d44bf9aacc8fdf5bdfb329a5ddbc90a38cd
Suhaib <suhaib92khan@gmail.com>
2020-03-30 00:18:33 -0400

    Finish lengthOfLongestSubstring problem

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -19,3 +19,42 @@ function twoSum(nums, target) { // [-1,-2,-3,-4,-5], -8
 // console.log(twoSum([2, 7, 11, 15], 9)); // => [0, 1]
 // console.log(twoSum([3, 2, 4], 6)); // => [1, 2]
 // console.log(twoSum([-1, -2, -3, -4, -5], -8)); // => [2, 4]
+
+// Given a string, find the length of the longest substring without repeating characters.
+// Example 1:
+// Input: "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+// Input: "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+// Input: "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+function lengthOfLongestSubstring(str) { // "abcabcbb"
+  if (!str.length) return 0;
+  let longest = 0;
+  let currentSubstr = '';
+
+  for (let i = 0; i < str.length; i++) {
+    currentSubstr = str.slice(i, i + 1)
+    for (let j = i + 1; j < str.length; j++) {
+      if (currentSubstr.includes(str[j])) {
+        break
+      }
+      currentSubstr = str.slice(i, j + 1);
+      if (currentSubstr.length > longest) longest = currentSubstr.length;
+    }
+  }
+  
+  return longest > 0 ? longest : 1; 
+}
+
+
+// console.log(lengthOfLongestSubstring("abcabcbb")); // => 3
+// console.log(lengthOfLongestSubstring("bbbbb")); // => 1
+// console.log(lengthOfLongestSubstring("pwwkew")); // => 3
