ae846cfb604a5c4da4bb6fe8c7e2f380c83f2978
Suhaib <suhaib92khan@gmail.com>
2020-03-29 23:27:16 -0400

    Finish twoSum problem

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -5,8 +5,17 @@
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1]
 
-function twoSum(nums, target) {
-  
+function twoSum(nums, target) { // [-1,-2,-3,-4,-5], -8
+  let sums = []; 
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target && j !== i) sums.push(...[i, j]) // 2 + 7 === 9, [].concat([0, 1])
+    }
+  }  
+
+  return Array.from(new Set(sums));
 }
 
-twoSum([2, 7, 11, 15], 9) // => [0, 1]
+// console.log(twoSum([2, 7, 11, 15], 9)); // => [0, 1]
+// console.log(twoSum([3, 2, 4], 6)); // => [1, 2]
+// console.log(twoSum([-1, -2, -3, -4, -5], -8)); // => [2, 4]
