58e73cffc64609a5f97d4cea3c7c5ebecf6a16df
Suhaib <suhaib92khan@gmail.com>
2020-04-06 16:03:05 -0400

    Finish smallerNumbersThanCurrent problem

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -54,18 +54,32 @@ function lengthOfLongestSubstring(str) { // "abcabcbb"
   return longest > 0 ? longest : 1; 
 }
 
+// Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+// That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+// Return the answer in an array.
 
-// console.log(lengthOfLongestSubstring("abcabcbb")); // => 3
-// console.log(lengthOfLongestSubstring("bbbbb")); // => 1
-// console.log(lengthOfLongestSubstring("pwwkew")); // => 3
+function smallerNumbersThanCurrent(nums) {
+  // Initial strategy is that I can use a nested loop and a counter and push the counter at the end into new array
 
-// Given a string s, find the longest palindromic substring in s.You may assume that the 
-// maximum length of s is 1000.
-// Example 1:
-// Input: "babad"
-// Output: "bab"
-// Note: "aba" is also a valid answer.
-// Example 2:
-// Input: "cbbd"
-// Output: "bb"
+  let counter = 0;
+  let counterArr = [];
 
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] > nums[j] && i !== j) {
+        counter += 1;
+      }
+    }
+    counterArr.push(counter);
+    counter = 0;
+  }
+
+  return counterArr;
+}
+
+// Time complexity is probably O(n^2) because of nested loops
+// Space complexity is probably O(n) linear because the same variable is used and manipulated in loops
+
+console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3])); // => [4,0,1,1,3]
+console.log(smallerNumbersThanCurrent([6, 5, 4, 8])); // => [2,1,0,3]
+console.log(smallerNumbersThanCurrent([7, 7, 7, 7])); // => [0,0,0,0]
