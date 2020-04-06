fe719b63d15b193c8c1e6c3da4c12d9b51b0942f
Suhaib <suhaib92khan@gmail.com>
2020-04-06 16:34:05 -0400

    Start defangIPaddr problem

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -111,3 +111,13 @@ function sumOfDigits(n) { // 234 = 9
 // Constraints 1 <= n <= 10^5
 // console.log(subtractProductAndSum(234)); // => 15
 // console.log(subtractProductAndSum(4421)); // => 21
+
+// Given a valid(IPv4) IP address, return a defanged version of that IP address.
+// A defanged IP address replaces every period "." with "[.]".
+
+function defangIPaddr(address) {
+
+}
+
+console.log(defangIPaddr("1.1.1.1")); // => "1[.]1[.]1[.]1"
+console.log(defangIPaddr("255.100.50.0")); // => "255[.]100[.]50[.]0"
