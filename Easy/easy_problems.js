50504e3a8df4ade569b7d6556e72f0009feec533
Suhaib <suhaib92khan@gmail.com>
2020-04-10 23:59:14 -0400

    Set up isPalindrome problem

---
+++ Easy/easy_problems.js
@@ -0,0 +1,9 @@
+// Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+// Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+function isPalindrome(str) {
+
+}
+
+console.log(isPalindrome("A man, a plan, a canal: Panama")); // => true
+console.log(isPalindrome("race a car")); // => false
