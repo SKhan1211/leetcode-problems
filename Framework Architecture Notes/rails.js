35dfc8d54eb4fe289740090f3904b296d15b2fee
Suhaib <suhaib92khan@gmail.com>
2020-04-11 19:17:05 -0400

    Add another tip

--- Framework Architecture Notes/rails.js
+++ Framework Architecture Notes/rails.js
@@ -2,3 +2,6 @@
 
 // If you want to position a child element right at top and left of parent, give parent position relative and give child a position absolute with top 0 left 0,
 //  should fit like a charm
+
+// If you call this anywhere in your function in React, you must bind it. If you don't, you don't need to bind but it wouldn't hurt to do it anyways for all to 
+//  avoid dumb bugs.
