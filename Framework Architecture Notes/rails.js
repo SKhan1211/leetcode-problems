d3fea10a3111edc272c5c32e90a93d1e34bd01c2
Suhaib <suhaib92khan@gmail.com>
2020-04-11 19:12:43 -0400

    Add another tip

--- Framework Architecture Notes/rails.js
+++ Framework Architecture Notes/rails.js
@@ -1,1 +1,4 @@
 // When using componentDidUpdate, it takes in prevProps or prevState as args and I can use this as conditionals for when an update should do something.
+
+// If you want to position a child element right at top and left of parent, give parent position relative and give child a position absolute with top 0 left 0,
+//  should fit like a charm
