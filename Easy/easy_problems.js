3e0c4f71d6fce0b5ecbcd1e2127353d6063211f0
Suhaib <suhaib92khan@gmail.com>
2020-04-11 00:18:42 -0400

    Finish isPalindrome problem

--- Easy/easy_problems.js
+++ Easy/easy_problems.js
@@ -2,8 +2,15 @@
 // Note: For the purpose of this problem, we define empty string as valid palindrome.
 
 function isPalindrome(str) {
-
+  let alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let chars = str.toLowerCase().split("").filter(el => alphanumeric.includes(el));
+  return chars.join("") === chars.reverse().join("");
 }
+
+// Time complexity most likely O(n) because of the .split and .filter methods creating O(n + n)
+// Space complexity is O(n) because the size of chars depends on input
 
 console.log(isPalindrome("A man, a plan, a canal: Panama")); // => true
 console.log(isPalindrome("race a car")); // => false
+console.log(isPalindrome("0P")); // => false
+
