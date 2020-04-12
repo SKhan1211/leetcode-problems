a5368dfc590d5b595c9d5aa7079b008c35e5852e
Suhaib <suhaib92khan@gmail.com>
2020-04-11 20:48:22 -0400

    Add futher tips

--- Framework Architecture Notes/rails.js
+++ Framework Architecture Notes/rails.js
@@ -5,3 +5,7 @@
 
 // If you call this anywhere in your function in React, you must bind it. If you don't, you don't need to bind but it wouldn't hurt to do it anyways for all to 
 //  avoid dumb bugs.
+
+// addEventListener functions can take in optional params, such as options that allow you to run an event listener only once. This would be called such as:
+//  document.addEventListener('click', this.handleSubmit, { once: true}), meaning the event listener will only run once. Check out MDN docs to see what all
+//  options may be passed in!
