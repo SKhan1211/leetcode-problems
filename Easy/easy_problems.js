bd207bde9ef1ddbd52625a657a6255c2d157a504
Suhaib <suhaib92khan@gmail.com>
2020-04-13 18:40:59 -0400

    Add alternative methods for numberOfSteps

--- Easy/easy_problems.js
+++ Easy/easy_problems.js
@@ -187,3 +187,10 @@ function numberOfSteps(num) {
 // console.log(numberOfSteps(14)); // => 6
 // console.log(numberOfSteps(8)); // => 4
 // console.log(numberOfSteps(123)); // => 12 
+
+//Alternative methods:
+// var numberOfSteps = function(num, count = 0) {
+//   if (num === 0) return count;
+//   return num % 2 === 0 ? numberOfSteps(num / 2, count + 1) : numberOfSteps(num - 1, count + 1);
+// }
+
