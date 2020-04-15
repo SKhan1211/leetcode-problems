b8987c68b82170346e08ab10933748e8a279ea0d
Suhaib <suhaib92khan@gmail.com>
2020-04-14 20:38:10 -0400

    Finish insertionSort

--- Algorithms/algorithms_practice.js
+++ Algorithms/algorithms_practice.js
@@ -44,7 +44,17 @@ function swap(arr, index1, index2) {
 // console.log(swap([3, 1], 0, 1)); // => [1, 3]
 
 function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentEle = arr[i]; 
 
+    for (var j = i - 1; j >= 0 && currentEle < arr[j]; j--) {
+      arr[j + 1] = arr[j];
+    };
+
+    arr[j + 1] = currentEle;
+  }
+
+  return arr;
 };
 
 console.log(insertionSort([1, 5, 2, 4, 6, 8])); // => [1, 2, 4, 5, 6, 8]
