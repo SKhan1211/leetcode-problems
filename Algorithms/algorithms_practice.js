e6043ebbf9dab806468451827cc680272fef71b6
Suhaib <suhaib92khan@gmail.com>
2020-04-14 20:21:18 -0400

    Setup selectionSort function

--- Algorithms/algorithms_practice.js
+++ Algorithms/algorithms_practice.js
@@ -15,4 +15,10 @@ function BubbleSort(array) {
   return array;
 }
 
-console.log(BubbleSort([4, 1, 3, 4, 6])); // => [1, 3, 4, 4, 6]
+// console.log(BubbleSort([4, 1, 3, 4, 6])); // => [1, 3, 4, 4, 6]
+
+function selectionSort(array) {
+
+};
+
+console.log(selectionSort([1, 45, 6, 3, 5])); // => [1, 3, 5, 6, 45];
