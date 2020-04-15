0f4f98aedd6adc830a4bb288d82236f96e345ab1
Suhaib <suhaib92khan@gmail.com>
2020-04-14 21:06:56 -0400

    Create helper methods for radixSort

--- Algorithms/algorithms_practice.js
+++ Algorithms/algorithms_practice.js
@@ -118,5 +118,14 @@ function radixSortWithNegatives(arr) {
 
 };
 
+const getDigitFrom = (num, place) => Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+const getIntLength = (num) => (num === 0) ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
+function getMaxDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, getIntLength(nums[i]));
+  }
+}
+
 console.log(radixSort([6, 2, 5, 2, 1])); // => [1, 2, 2, 5, 6]
 console.log(radixSort([4, 6, 2, -5, 7, -1])); // => [-5, -1, 2, 4, 6, 7]
