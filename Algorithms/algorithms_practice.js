8d1481e1086c1220563e0a94f4fbe3bf33fb02ce
Suhaib <suhaib92khan@gmail.com>
2020-04-14 20:19:33 -0400

    Finish BubbleSort iterative

--- Algorithms/algorithms_practice.js
+++ Algorithms/algorithms_practice.js
@@ -1,7 +1,18 @@
 // Bubble Sort
 
 function BubbleSort(array) {
+  let swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        swapped = true;
+      };
+    };
+  };
 
+  return array;
 }
 
 console.log(BubbleSort([4, 1, 3, 4, 6])); // => [1, 3, 4, 4, 6]
