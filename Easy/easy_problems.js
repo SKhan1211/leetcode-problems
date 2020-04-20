251ebd6e68af02924bc43aed566ff5d559814010
Suhaib <suhaib92khan@gmail.com>
2020-04-20 16:14:36 -0400

    Finish freqAlphabet problem

--- Easy/easy_problems.js
+++ Easy/easy_problems.js
@@ -676,3 +676,29 @@ function RAndLCount(str) {
 
 //   return matches;
 // };
+
+function freqAlphabets(s) {
+  let charMapping = {
+    '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f', '7': 'g', '8': 'h', '9': 'i',
+    '10#': 'j', '11#': 'k', '12#': 'l', '13#': 'm', '14#': 'n', '15#': 'o', '16#': 'p',
+    '17#': 'q', '18#': 'r', '19#': 's', '20#': 't', '21#': 'u', '22#': 'v', '23#': 'w', 
+    '24#': 'x', '25#': 'y', '26#': 'z',
+  }
+
+  let splitNums = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i + 2] === "#") {
+      splitNums.push(s.slice(i, i + 3));
+      i = i + 2;
+    } else {
+      splitNums.push(s[i]);
+    }
+  }
+
+  return splitNums.map(numStr => charMapping[numStr]).join('');
+};
+
+// Time complexity: O(n) because we iterate but never nested
+// Space complexity: O(n) because splitNum size worst case could be all numbers less than 9 so dependant on input size
+
+// console.log(freqAlphabets("10#11#12")); // => "jkab" 
