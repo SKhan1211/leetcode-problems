bfb313b46faee6c00d1ad4e86ef0c4e5407d0548
Suhaib <suhaib92khan@gmail.com>
2020-04-21 15:59:21 -0400

    Finish numUniqueEmails problem

--- Easy/easy_problems.js
+++ Easy/easy_problems.js
@@ -776,3 +776,78 @@ function sortedSquares(A) {
 // Space complexity: O(C) because no space is used just manipulated
 
 // console.log(sortedSquares([-4, -1, 0, 3, 10])); // => [0,1,9,16,100]
+// Alternative Solutions:
+// var sortedSquares = function(A) {
+//   const len = A.length;
+//   const squares = Array(len);
+//   let start = 0;
+//   let end = len - 1;
+
+//   for (let i = end; i >= 0; i--) {
+//     const sq1 = A[start] ** 2;
+//     const sq2 = a[end] ** 2;
+
+//     if (sq1 > sq2) {
+//       squares[i] = sq1;
+//       start++;
+//     } else {
+//       squares[i] = sq2;
+//       end--;
+//     }
+//   }
+
+//   return squares;
+// };
+
+// var sortedSquares = function(A) {
+//   let result = [];
+//   let l = 0;
+//   let r = A.length - 1;
+//   let p = r;
+
+//   while (l <= r) {
+//     if (A[l] ** 2 > A[r] ** 2) {
+//       result[p--] = A[l++] ** 2;
+//     } else {
+//       result[p--] = A[r--] ** 2;
+//     }
+//   }
+
+//   return result;
+// }
+
+// var sortedSquares = function (A) {
+//   A = A.map(x => x * x);
+//   A.sort((a, b) => a - b);
+
+//   return A;
+// };
+
+function numUniqueEmails(emails) {
+  let emailSet = new Set();
+  emails.forEach(email => { // O(n)
+    let newEmail = '';
+    let localName = email.slice(0, email.indexOf("@"));
+    let domainName = email.slice(email.indexOf("@"));
+
+    localName = localName.split("").map(char => { // O(n^2)
+      if (char === ".") return "";
+      else return char;
+    }).join("");
+
+    if (localName.includes("+")) {
+      localName = localName.slice(0, localName.indexOf("+"));
+    };
+
+    newEmail += localName + domainName;
+
+    emailSet.add(newEmail);
+  });
+
+  return emailSet.size;
+};
+
+// Time Complexity: O(n^2) because of nested loops
+// Space Complexity: O(n) because worst case it adds all letters
+
+console.log(numUniqueEmails(["test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"])); // => 2
