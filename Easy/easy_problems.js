7b83ca62a4bc7c22e5fddaa6b19f16779dbe5989
Suhaib <suhaib92khan@gmail.com>
2020-05-12 21:27:43 -0400

    Add alternative solutions to firstUniqChar

--- Easy/easy_problems.js
+++ Easy/easy_problems.js
@@ -1749,3 +1749,21 @@ function firstUniqChar(s) {
 // console.log(firstUniqChar("leetcode")); // => 0
 // console.log(firstUniqChar("loveleetcode")); // => 2
 
+// Alternative solutions:
+// var firstUniqChar = function(s) {
+//   let map = {};
+//   for (let char of s) {
+//     map[char] ? map[char]++ : map[char] = 1;
+//   }
+//   for (let i = 0; i < s.length; i++) {
+//     if (map[s[i]] === 1) return i; 
+//   };
+//   return -1;
+// }
+// var firstUniqChar = function(s) {
+//   for (i = 0; i < s.length; i++) {
+//     if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i;
+//   }
+
+//   return -1;
+// }
